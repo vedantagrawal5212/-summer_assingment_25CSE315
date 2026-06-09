d4862e50e326e878d029e301e78d3c365907d508
vedantagrawal5215 <vedantagrawal546@gmail.com>
2026-06-09 21:29:31 +0530

    Question 1 done

---
+++ Day1/Question 1.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+Question 1 done
+int main() {
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    /* Handle 0 as a special case */
+    if (n == 0) {
+        count = 1;
+    } else {
+        /* Make n positive for negative numbers */
+        if (n < 0) {
+            n = -n;
+        }
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits = %d\n", count);
+
+    return 0;
+}
