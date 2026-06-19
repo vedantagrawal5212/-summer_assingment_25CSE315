9edb7a492276d003d7645fd7736bc02f265b3e21
vedantagrawal5215 <vedantagrawal546@gmail.com>
2026-06-19 15:25:10 +0530

    Find factorial

---
+++ Day 11/Question 44.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+long long factorial(int n)
+{
+    long long fact = 1;
+    int i;
+
+    for (i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Factorial = %lld", factorial(num));
+
+    return 0;
+}
