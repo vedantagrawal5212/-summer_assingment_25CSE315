21a52c948583538429812f18ec96aabcf6b47fd6
vedantagrawal5215 <vedantagrawal546@gmail.com>
2026-06-19 15:17:25 +0530

    Sum of two numbers

---
+++ Day 11/Question 41.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Sum = %d", sum(num1, num2));
+
+    return 0;
+}
