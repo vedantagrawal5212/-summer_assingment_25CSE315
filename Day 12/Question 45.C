8f4c1e1005d1e17faca0687be1598faf8fbbb6de
vedantagrawal5215 <vedantagrawal546@gmail.com>
2026-06-20 15:12:36 +0530

    Palindrome number

---
+++ Day 12/Question 45.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int armstrong(int n)
+{
+    int sum = 0, rem, temp = n;
+
+    while (temp != 0)
+    {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    return (sum == n);
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (armstrong(num))
+        printf("%d is an Armstrong Number", num);
+    else
+        printf("%d is Not an Armstrong Number", num);
+
+    return 0;
+} 
