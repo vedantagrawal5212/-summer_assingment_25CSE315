a5f115d9db8c3971ac69888569d8caec5a188d9a
vedantagrawal5215 <vedantagrawal546@gmail.com>
2026-07-06 18:28:45 +0530

    Check string rotation

--- Day 24/Question 93.C
+++ Day 24/Question 93.C
@@ -1,9 +1,7 @@
 public class StringRotation {
-
     public static boolean isRotation(String s1, String s2) {
-        if (s1.length() != s2.length()) {
+        if (s1.length() != s2.length())
             return false;
-        }
 
         String temp = s1 + s1;
         return temp.contains(s2);
@@ -13,10 +11,9 @@ public class StringRotation {
         String s1 = "ABCD";
         String s2 = "CDAB";
 
-        if (isRotation(s1, s2)) {
+        if (isRotation(s1, s2))
             System.out.println("String is a rotation.");
-        } else {
+        else
             System.out.println("String is not a rotation.");
-        }
     }
 }
